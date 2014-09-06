bd7c3082a8ee359a6394d262d51328e1a67788c9
Jared Hanson <jaredhanson@gmail.com>
2014-09-05 22:37:29 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('bixby', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
