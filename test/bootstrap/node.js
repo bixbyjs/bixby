b4f77a0321336a98bf11030efbd3ebd3770c8ba3
Jared Hanson <jaredhanson@gmail.com>
2023-01-06 09:49:39 -0800

    Add tests.

--- test/bootstrap/node.js
+++ test/bootstrap/node.js
@@ -1,3 +1,3 @@
 var chai = require('chai');
 
-global.expect = chai.expect;
+chai.use(require('sinon-chai'));
--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,25 @@
 /* global describe, it, expect */
 
 var bixby = require('..');
+var expect = require('chai').expect;
+var sinon = require('sinon');
 
 describe('bixby', function() {
   
-  it('should export component suites', function() {
-    expect(bixby.common).to.be.a('function');
+  it('should export function', function() {
+    expect(bixby).to.be.a('function');
+  });
+  
+  it('should create app', function(done) {
+    var container = new Object();
+    container.create = sinon.stub().resolves();
+    
+    bixby(container)
+      .then(function() {
+        expect(container.create).to.have.been.calledOnceWith('app');
+        done();
+      })
+      .catch(done);
   });
   
 });
