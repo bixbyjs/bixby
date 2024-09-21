96c6da92bbfb6605240caefa1c4e933fc378e2c5
Jared Hanson <jaredhanson@gmail.com>
2024-09-21 10:17:15 -0700

    Add tests.

--- test/variables/uri.test.js
+++ test/variables/uri.test.js
@@ -9,8 +9,12 @@ describe('variables/uri', function() {
   
   it('should resolve using service annotation', function(done) {
     var resolver = new Object();
-    resolver.resolve = sinon.stub().yieldsAsync(null, [
-      { uri: 'ftp://ftp1.example.com/public' }
+    resolver.resolve = sinon.stub();
+    resolver.resolve.onCall(0).yieldsAsync(null, [
+      { url: 'ftp://ftp1.example.com/public' }
+    ]);
+    resolver.resolve.onCall(1).yieldsAsync(null, [
+      { url: 'ftp://ftp1.example.com/public' }
     ]);
     var C = new Object();
     C.create = sinon.stub().resolves(resolver);
@@ -23,10 +27,15 @@ describe('variables/uri', function() {
     factory.call(C, 'foo')
       .then(function(uri) {
         expect(C.create).to.have.been.calledOnceWith('http://i.bixbyjs.org/ns/Resolver');
-        expect(resolver.resolve).to.have.been.calledOnceWith('_foo._tcp', 'URI');
+        expect(resolver.resolve.getCall(0).args[0]).to.equal('_foo._tcp');
+        expect(resolver.resolve.getCall(0).args[1]).to.equal('SRV');
+        expect(resolver.resolve.getCall(1).args[0]).to.equal('_foo._tcp');
+        expect(resolver.resolve.getCall(1).args[1]).to.equal('URI');
+        expect(resolver.resolve.callCount).to.equal(2);
         expect(uri).to.equal('ftp://ftp1.example.com/public');
         done();
-      });
+      })
+      .catch(done);
     
   }); // should resolve using service annotation
   
